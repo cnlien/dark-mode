b1f2aab7ece48e30455fbce1633086fcdd665029
Christopher Lien <cnlien@gmail.com>
2019-11-05 16:46:29 -0500

    added hooks directory. useLocalStorage is complete. Still working on useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  { useLocalStorage } from './useLocalStorage';
+
+const useDarkMode = (key, initialValue) => {
+
+    const [value, setValue] = useLocalStorage(key, initialValue);
+    const handleChanges = updatedValue => {
+        setValue(updatedValue);
+    };
+    return [value, setValue, handleChanges];
+
+}
+
+export default useDarkMode;
