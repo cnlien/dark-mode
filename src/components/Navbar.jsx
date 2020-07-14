50c39504bdfed969b2bf8595dd69548e9bf92431
Chris Lien <clien@sharpencx.com>
2020-07-14 15:30:23 -0400

    Original Lambda Repo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,19 @@
-import React from 'react';
-import { useDarkMode } from '../hooks/useDarkMode';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useDarkMode("darkmode", "true");
-
-  console.log("darkMode from Navbar.jsx is", darkMode, "because it is the opposite from useEffect")
-
+  const [darkMode, setDarkMode] = useState(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
-    console.log("Ran toggleMode from Navbar.jsx and set state of darkMode to:", darkMode)
-    document.body.classList.toggle("dark-mode");
   };
-
-  const darkModeLabel = {
-    marginLeft: 'auto',
-    marginRight: '10px',
-  };
-
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <h4 style={darkModeLabel}>Toggle Dark Mode</h4>
       <div className="dark-mode__toggle">
-        <div onClick={toggleMode} className={darkMode ? 'toggle toggled' : 'toggle'} />
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
       </div>
     </nav>
   );
